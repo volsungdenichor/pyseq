a168ba32911670ba9e6a6590c6c7541992a6148f
Krzysztof Muhleisen <krzysztof.muhleisen@gmail.com>
2020-03-21 15:34:53 +0100

    partition

--- pyseq/seq.py
+++ pyseq/seq.py
@@ -194,6 +194,11 @@ class Seq:
         it1, it2 = itertools.tee(self._iterable)
         return Seq(it1), Seq(it2)
 
+    def partition(self, pred, post_op=None):
+        post_op = _if_none(post_op, identity)
+        s1, s2 = self.tee()
+        return tuple(post_op(x) for x in (s1.take_if(pred), s2.drop_if(pred)))
+
     def all(self, pred=None):
         return all(self.map(_if_none(pred, bool)))
 
